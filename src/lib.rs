93d4f6b48f81f482372bbeaeacb14b5c8009727f
James Simkins <131498226+james-simkins-pittsburgh@users.noreply.github.com>
2024-05-15 06:45:30 -0400

    Added DTrig struct
    Added the main struct of the library that holds the tables of trig values.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/* This is the main struct of the library. The data holds a bunch of tables of pre-calculated trig results. The precalculation
+avoids that potential problem of different results across architectures. */
+pub struct DTrig {
+    pub sine_array: [i16; 6283],
+    pub cosine_array: [i16; 6283],
+    pub tangent_array: [i16; 6283],
+    pub arcsine_array: [i16; 2001],
+    pub arccosine_array: [i16; 2001],
+    /* Size of artangent arrays is set based on the minimum spacing needed to give
+    thousandths place accuracy. */
+    pub arctanent_thousandths: [i16; 4001],
+    pub arctangent_hundreths: [i16; 2001],
+    pub arctanent_tenth: [i16; 1001],
+    pub arctangent_ones: [i16; 1001],
 }
