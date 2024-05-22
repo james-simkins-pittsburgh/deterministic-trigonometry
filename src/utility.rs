8253ea607b6eae4ab1ef2ae8c34f7cf9ace94333
James Simkins <131498226+james-simkins-pittsburgh@users.noreply.github.com>
2024-05-22 07:27:34 -0400

    Update utility.rs
    Working on bug that is making negative test fail.

--- src/utility.rs
+++ src/utility.rs
@@ -6,13 +6,23 @@ pub fn denominator_to_1000(argument_fraction: (i32, i32)) -> i64 {
     } else {
         // Handles other denominators with appropriate rounding to nearest thousandth.
         if argument_fraction.1 % 2 == 0 {
-            if (argument_fraction.0 * 1000) % argument_fraction.1 >= argument_fraction.1 / 2 {
-                return i64::from((argument_fraction.0 * 1000) / argument_fraction.1 + 1);
+            if
+                ((argument_fraction.0 * 1000) % argument_fraction.1).abs() >=
+                (argument_fraction.1 / 2).abs()
+            {
+                if argument_fraction.0 >= 0 {
+                    return i64::from((argument_fraction.0 * 1000) / argument_fraction.1 + 1);
+                } else {
+                    return i64::from((argument_fraction.0 * 1000) / argument_fraction.1 - 1);
+                }
             } else {
                 return i64::from((argument_fraction.0 * 1000) / argument_fraction.1);
             }
         } else {
-            if (argument_fraction.0 * 1000) % argument_fraction.1 > argument_fraction.1 / 2 {
+            if
+                ((argument_fraction.0 * 1000) % argument_fraction.1).abs() >
+                (argument_fraction.1 / 2).abs()
+            {
                 return i64::from((argument_fraction.0 * 1000) / argument_fraction.1 + 1);
             } else {
                 return i64::from((argument_fraction.0 * 1000) / argument_fraction.1);
@@ -76,27 +86,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_denominator_to_1000 () {
-
-    test_equal((8.0,1000.0), (8,1000));
-    test_equal((17.0,1000.0), (17,1000));
-    test_equal((3.0,7.0), (3,7));
-    // test_equal((i32::MAX as f64,1.0), (i32::MAX, 1)); // Right now this fails
-    // test_equal((i32::MIN as f64,1.0), (i32::MIN, 1)); // Right now this fails
-    test_equal((-4.0,1000.0), (-4,1000));
-    test_equal((-19.0,1000.0), (-19,1000));
-    test_equal((-3.0,11.0), (-3,11));
-
-
-
-
-
+    fn test_denominator_to_1000() {
+        test_equal((8.0, 1000.0), (8, 1000));
+        test_equal((17.0, 1000.0), (17, 1000));
+        test_equal((3.0, 7.0), (3, 7));
+        // test_equal((i32::MAX as f64,1.0), (i32::MAX, 1)); // Right now this fails
+        // test_equal((i32::MIN as f64,1.0), (i32::MIN, 1)); // Right now this fails
+        test_equal((-4.0, 1000.0), (-4, 1000));
+        test_equal((-19.0, 1000.0), (-19, 1000));
+        test_equal((-3.0, 11.0), (-3, 11));
     }
 
-    fn test_equal (float_fraction: (f64, f64), integer_fraction: (i32, i32)) {
-
-    assert_eq!((((float_fraction.0/float_fraction.1)*1000.0).round()) as i64, denominator_to_1000(integer_fraction))
-
+    fn test_equal(float_fraction: (f64, f64), integer_fraction: (i32, i32)) {
+        assert_eq!(
+            ((float_fraction.0 / float_fraction.1) * 1000.0).round() as i64,
+            denominator_to_1000(integer_fraction)
+        )
     }
-
 }
